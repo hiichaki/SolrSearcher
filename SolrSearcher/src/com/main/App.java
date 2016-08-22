2ef0c02baead2b30b2f83a89982b6efbb2ddc0b1
hiichaki <hiichaki350@gmail.com>
2016-08-22 13:18:23 +0300

    refactor

--- SolrSearcher/src/com/main/App.java
+++ SolrSearcher/src/com/main/App.java
@@ -1,7 +1,9 @@
 package com.main;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
 
 import org.apache.http.HttpException;
@@ -23,19 +25,13 @@ import org.apache.solr.common.SolrDocumentList;
 
 public class App {
 
-	public static void main(String[] args) throws SolrServerException, IOException {
-		String url = "";
-		String httpAuthUser = "";
-		String httpAuthPass = "";
-		// Configure latests Apache Http Components http client
-		HttpClient httpClient = new DefaultHttpClient();
-		if (httpAuthUser != null && httpAuthPass != null) {
-			AbstractHttpClient client = (AbstractHttpClient) httpClient;
-			App a = new App();
-			client.addRequestInterceptor(a.new PreEmptiveBasicAuthenticator(httpAuthUser, httpAuthPass));
-		}
-		SolrServer solr = new HttpSolrServer(url, httpClient);
-
+	
+	
+	/*
+	 * {%1 Alpha 2%}
+	 */
+	public static void getPattern1(SolrServer solr)
+			throws SolrServerException, FileNotFoundException, UnsupportedEncodingException {
 		SolrQuery query = new SolrQuery();
 		query.setQuery("description:/[0-9]/");
 		query.setFields("id", "category_id", "description");
@@ -43,11 +39,7 @@ public class App {
 		QueryResponse response = solr.query(query);
 		SolrDocumentList list = response.getResults();
 		Iterator<SolrDocument> si = list.iterator();
-		// System.out.println("Solr document" + list.getNumFound());
 
-		// String pattern = "^{%[a-zA-Z]*\\ {0,}\\d{1,3}\\ {0,}%}$";
-		// Pattern p = Pattern.compile(pattern);
-		// Create a Pattern object
 		PrintWriter writer = new PrintWriter("result.txt", "UTF-8");
 		while (si.hasNext()) {
 			String tmp = si.next().toString();
@@ -61,12 +53,6 @@ public class App {
 						int idEnd = tmp.indexOf(",", idBegin);
 
 						int categoryBegin = tmp.indexOf("category_id=");
-
-						// int categoryEnd = tmp.indexOf("}", categoryBegin -
-						// 1);
-						// if (categoryEnd > categoryBegin + 10) {
-						// categoryEnd = tmp.indexOf(",", categoryBegin - 1);
-						// }
 
 						String id = tmp.substring(idBegin, idEnd);
 						String category = tmp.substring(categoryBegin, categoryBegin + 13);
@@ -87,6 +73,89 @@ public class App {
 
 		}
 		writer.close();
+	}
+
+	/*
+	 * <em>*</em>
+	 */
+	public static void getPattern2(SolrServer solr)
+			throws SolrServerException, FileNotFoundException, UnsupportedEncodingException {
+		SolrQuery query = new SolrQuery();
+		query.setQuery("htmldescription:*");
+		query.setFields("id", "title", "category_id", "htmldescription");
+		query.setRows(2147483647);
+		QueryResponse response = solr.query(query);
+		SolrDocumentList list = response.getResults();
+		Iterator<SolrDocument> si = list.iterator();
+		PrintWriter writer = new PrintWriter("results.txt", "UTF-8");
+		while (si.hasNext()) {
+			String tmp = si.next().toString();
+			int beginIndex = tmp.indexOf("<em>");
+			int endIndex = tmp.indexOf("</em>", beginIndex);
+
+			if (beginIndex != endIndex) {
+				int idBegin = tmp.indexOf("id=");
+				int idEnd = tmp.indexOf(",", idBegin);
+
+				int categoryBegin = tmp.indexOf("category_id=");
+
+				int titleBegin = tmp.indexOf("title=");
+				int titleEnd = tmp.indexOf("}", titleBegin);
+
+				String id = tmp.substring(idBegin, idEnd);
+				String category = replaceCategory(tmp.substring(categoryBegin + 12, categoryBegin + 13));
+				String verse = tmp.substring(beginIndex, endIndex + 5);
+				String title = tmp.substring(titleBegin, titleEnd);
+				writer.write(String.format("%-9s %-10s %-82s %-20s%n ", id, category, title, verse));
+
+			}
+			beginIndex = tmp.indexOf("<em>", endIndex);
+			endIndex = tmp.indexOf("</em>", beginIndex);
+		}
+
+		writer.close();
+
+	}
+
+	public static SolrDocumentList getSolrList(SolrServer solr, SolrQuery query) throws SolrServerException {
+		QueryResponse response = solr.query(query);
+		SolrDocumentList list = response.getResults();
+		return list;
+	}
+
+	public static void getAnswer() {
+
+	}
+
+	public static String replaceCategory(String category) {
+		switch (category) {
+		case "1":
+			return "topic";
+		case "2":
+			return "media";
+		case "3":
+			return "location";
+		case "4":
+			return "time";
+		default:
+			return null;
+		}
+	}
+
+	public static void main(String[] args) throws SolrServerException, IOException {
+		String url = "";
+		String httpAuthUser = "";
+		String httpAuthPass = "";
+		// Configure latests Apache Http Components http client
+		HttpClient httpClient = new DefaultHttpClient();
+		if (httpAuthUser != null && httpAuthPass != null) {
+			AbstractHttpClient client = (AbstractHttpClient) httpClient;
+			App a = new App();
+			client.addRequestInterceptor(a.new PreEmptiveBasicAuthenticator(httpAuthUser, httpAuthPass));
+		}
+		SolrServer solr = new HttpSolrServer(url, httpClient);
+
+		getPattern2(solr);
 
 	}
 
